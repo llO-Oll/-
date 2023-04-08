364b4569f6b530195c02d8210f34fe86d3d768fa
Coupling <57003610+llO-Oll@users.noreply.github.com>
2023-04-08 21:42:30 +0800

    1

--- code_note/CPP/test.cpp
+++ code_note/CPP/test.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main(){
-
+1
     string a = "123"; // a是个左值
     string &ref_a_left = a; // 左值引用指向左值
     string &&ref_a_right = std::move(a); // 通过std::move将左值转化为右值，可以被右值引用指向
